9c41958adae0bdee13fac0610e935334d19c9f86
chushaa <31249563+chushaa@users.noreply.github.com>
2020-08-13 12:11:21 -0500

    Initial commit

---
+++ TestDrivenDevelopment/BlogTests/BlogTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BlogTests
+{
+    public class BlogTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TestDrivenDevelopment/Math Test/MathTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathTests
+{
+    [TestClass]
+    public class MathTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
