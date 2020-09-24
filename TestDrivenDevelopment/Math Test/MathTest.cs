38397c25b977b6e599201cf8dc5fd5dbe1bfeae7
EduardL <eduardl@magenic.com>
2020-09-24 10:52:02 +0800

    homework for TDD Section 2

--- TestDrivenDevelopment/Math Test/MathTest.cs
+++ TestDrivenDevelopment/Math Test/MathTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyMath;
+using System;
 
 namespace MathTests
 {
@@ -10,6 +11,48 @@ namespace MathTests
         public void BasicRooterTest()
         {
             Rooter rooter = new Rooter();
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+
+            double actualResult = rooter.SquareRoot(input);
+
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+        }
+
+        [TestMethod]
+        public void RooterValueChange()
+        {
+            Rooter rooter = new Rooter();
+
+            for (double expected = 1e-8; expected < 1e+8; expected *= 3.2)
+            {
+                RooterOneValue(rooter, expected);
+            }
+        }
+
+        private void RooterOneValue(Rooter rooter, double expectedResult)
+        {
+            double input = expectedResult * expectedResult;
+            double actualResult = rooter.SquareRoot(input);
+
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+        }
+
+        [TestMethod]
+        public void RooterTestNegativeInputs()
+        {
+            Rooter rooter = new Rooter();
+
+            try
+            {
+                rooter.SquareRoot(-10);
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+                return;
+            }
+
+            Assert.Fail();
         }
     }
 }
