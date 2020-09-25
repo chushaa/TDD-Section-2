5301f16a2538c2cd9f6ab4dd4a9f1d243e0abcbb
Aura Natividad <AuraN@magenic.com>
2020-09-25 17:05:29 +0800

    TDD Section 2 Homework_Auracedes Natividad

--- TestDrivenDevelopment/BlogTests/BlogTest.cs
+++ TestDrivenDevelopment/BlogTests/BlogTest.cs
@@ -5,15 +5,35 @@ using Xunit;
 using MyBlog;
 using MyBlog.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace BlogTests
 {
     public class BlogTest
     {
+        private readonly BlogController blogController;
+        private Mock<List<Post>> mockPostsList;
+
+        public BlogTest()
+        {
+            mockPostsList = new Mock<List<Post>>();
+            blogController = new BlogController(mockPostsList.Object);
+        }
+
         [Fact]
         public void GetTest_ReturnsListsOfPosts()
         {
+            var mockPosts = new List<Post>
+            {
+                new Post{Title = "TDD One"},
+                new Post{Title = "TDD and BDD"}
+            };
 
+            mockPostsList.Object.AddRange(mockPosts);
+
+            var result = blogController.Get();
+            var model = Assert.IsAssignableFrom<ActionResult<List<Post>>>(result);
         }
     }
 }
