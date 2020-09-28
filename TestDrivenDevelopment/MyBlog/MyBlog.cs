15ef9fa5ffa10e7804e5fc6c8f98785557e780ff
MAGENIC\RalphG <ralphg@magenic.com>
2020-09-28 13:58:28 +0800

    TDD Section 2 Homework_RalphGonzales

--- TestDrivenDevelopment/MyBlog/MyBlog.cs
+++ TestDrivenDevelopment/MyBlog/MyBlog.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyBlog
 {
+
     public class Post
     {
+        public string Title { get; set; }
+
+        public Guid Id { get; set; }
+
+        public string Content { get; set; }
     }
 }
