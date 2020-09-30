e9d4e032a5643ede58bc8e11347fa20a3bbdb50b
Runette Orobia <RunetteO@magenic.com>
2020-09-30 15:37:31 +0800

    TDD Section 2 Homework Runette Orobia

--- TestDrivenDevelopment/Math Test/MathTest.cs
+++ TestDrivenDevelopment/Math Test/MathTest.cs
@@ -10,6 +10,47 @@ namespace MathTests
         public void BasicRooterTest()
         {
             Rooter rooter = new Rooter();
+
+            double expectedResult = 2.0;
+
+            double input = expectedResult * expectedResult;
+
+            double actualResult = rooter.SquareRoot(input);
+
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+        }
+
+        [TestMethod]
+        public void RooterValueRange()
+        {
+            Rooter rooter = new Rooter();
+            for (double expected = 1e-8; expected < 1e+8; expected *= 3.2)
+            {
+                RooterOneValue(rooter, expected);
+            }
+        }
+
+        private void RooterOneValue(Rooter rooter, double expectedResult)
+        {
+            double input = expectedResult * expectedResult;
+            double actualResult = rooter.SquareRoot(input);
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 1000);
+        }
+
+        [TestMethod]
+        public void RooterTestNegativeInput()
+        {
+            Rooter rooter = new Rooter();
+            try
+            {
+                rooter.SquareRoot(-10);
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+                return;
+            }
+
+            Assert.Fail();
         }
     }
 }
