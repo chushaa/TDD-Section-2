ad7951118ce42ada2ee1ed9922a03273c0b7e823
John Ephraim Tugado <johnt@magenic.com>
2020-10-01 17:49:41 +0800

    Tugado, John Ephraim TDD Section 2 Homework

--- TestDrivenDevelopment/BlogTests/BlogTest.cs
+++ TestDrivenDevelopment/BlogTests/BlogTest.cs
@@ -10,10 +10,28 @@ namespace BlogTests
 {
     public class BlogTest
     {
+        private readonly BlogController blogController;
+        private Mock<List<Post>> mockPostsList;
+
+        public BlogTest()
+        {
+            mockPostsList = new Mock<List<Post>>();
+            blogController = new BlogController(mockPostsList.Object);
+        }
+
         [Fact]
         public void GetTest_ReturnsListsOfPosts()
         {
+            var mockPosts = new List<Post>
+            {
+                new Post() { Title = "TDD One" },
+                new Post() { Title = "TDD and BDD" }
+            };
 
+            mockPostsList.Object.AddRange(mockPosts);
+
+            var result = blogController.Get();
+            var model = Assert.IsAssignableFrom<ActionResult<List<Post>>>(result);
         }
     }
 }
--- TestDrivenDevelopment/MyBlog/MyBlog.cs
+++ TestDrivenDevelopment/MyBlog/MyBlog.cs
@@ -7,5 +7,10 @@ namespace MyBlog
 {
     public class Post
     {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
     }
 }
